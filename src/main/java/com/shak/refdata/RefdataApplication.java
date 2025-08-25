f93a12cc9e006c77b24a52ea39c8790b058bc24e
Akshay <akshaynimbekar1999@gmail.com>
2025-08-25 17:24:58 +0530

    Initial project scaffold with Maven, folder structure, and application.yaml

---
+++ src/main/java/com/shak/refdata/RefdataApplication.java
@@ -0,0 +1,11 @@
+package com.shak.refdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefdataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RefdataApplication.class, args);
+    }
+}
